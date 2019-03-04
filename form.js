16d85c293cf4ae55c2b317aee7a5384c7fd5524d
Demenyaca <demenyaca@gmail.com>
2019-03-04 13:35:14 -0500

    completed

--- form.js
+++ form.js
@@ -1,10 +1,15 @@
-<script>
-document.getElementById("sub1").addEventListener("click", displayThankYou);
 
-function displayThankYou() {
-  document.getElementById("sub2").innerHTML = ThankYou();
+document.getElementById("sub1").addEventListener("click", displayThankYou!);
 
-</script>
+function displayThankYou(){
+  alert("Thank You!")
+
+   
+  document.getElementById("sub2").innerHTML ="ThankYou!";
+} 
+
+
+
 // Click submit get message thank you!
 // complete contact info click submit takes you to a "thank you" page
 
