ab8524330fb8824ba04ccb3d14376287bd95c039
Demenyaca Morton <demenyaca@gmail.com>
2019-03-03 23:35:45 -0500

    EventListner practice

---
+++ form.js
@@ -0,0 +1,10 @@
+<script>
+document.getElementById("sub1").addEventListener("click", displayThankYou);
+
+function displayThankYou() {
+  document.getElementById("sub2").innerHTML = ThankYou();
+
+</script>
+// Click submit get message thank you!
+// complete contact info click submit takes you to a "thank you" page
+
