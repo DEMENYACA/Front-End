4b35c2c0b439e3cf7eb62302b636d4407d1b6364
DEMENYACA <39350941+DEMENYACA@users.noreply.github.com>
2019-03-18 15:37:13 -0400

    PHP GET and POST

---
+++ PHP/display.php
@@ -0,0 +1,23 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<!-- Added for  <HTML></HTML["name"]; ?> -->
+Your email is <?php echo $_POST ["email"] ; ?><br>
+
+<br>Your address is <?php echo $_POST["address"]; ?><br>
+<br>Your phone is <?php echo $_POST["phone"];?><br>
+
+<br>What is your favorite holiday <?php echo $_POST['holiday'];?><br>
+<br>What is your son's name  <?php echo $_POST[ 'name'];?><br>
+
+
+<br>What is your favorite color <?php echo $_POST['color'];?>
+
+</body>
+</html>
+ 
---
+++ PHP2/form.php
@@ -0,0 +1,3 @@
+<form action= "form.php" method="get">
+First name: <input type="text" name="fname">
+</form>
